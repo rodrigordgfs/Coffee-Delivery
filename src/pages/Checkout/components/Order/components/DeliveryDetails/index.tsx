5e9ecf5aeca98be3ed746f8128189842a1709c87
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-02-10 18:57:26 -0300

    Bugfix: Corrigido problema de não manter os dados no formulario de endereço.

--- src/pages/Checkout/components/Order/components/DeliveryDetails/index.tsx
+++ src/pages/Checkout/components/Order/components/DeliveryDetails/index.tsx
@@ -10,7 +10,7 @@ import { useContext } from "react";
 import { OrderContext } from "../../../../../../context/Order";
 
 export function DeliveryDetails() {
-  const { fillAddress } = useContext(OrderContext);
+  const { address, fillAddress } = useContext(OrderContext);
 
   return (
     <DeliveryDetailsWrapper>
@@ -24,30 +24,37 @@ export function DeliveryDetails() {
       <DeliveryDetailsForm>
         <DeliveryDetailsFormInput
           placeholder="CEP"
+          value={address.CEP}
           onChange={(e) => fillAddress({ CEP: e.target.value })}
         />
         <DeliveryDetailsFormInput
           placeholder="Rua"
+          value={address.rua}
           onChange={(e) => fillAddress({ rua: e.target.value })}
         />
         <DeliveryDetailsFormInput
           placeholder="Numero"
+          value={address.numero}
           onChange={(e) => fillAddress({ numero: e.target.value })}
         />
         <DeliveryDetailsFormInput
           placeholder="Complemento"
+          value={address.complemento}
           onChange={(e) => fillAddress({ complemento: e.target.value })}
         />
         <DeliveryDetailsFormInput
           placeholder="Bairro"
+          value={address.bairro}
           onChange={(e) => fillAddress({ bairro: e.target.value })}
         />
         <DeliveryDetailsFormInput
           placeholder="Cidade"
+          value={address.cidade}
           onChange={(e) => fillAddress({ cidade: e.target.value })}
         />
         <DeliveryDetailsFormInput
           placeholder="Estado"
+          value={address.estado}
           onChange={(e) => fillAddress({ estado: e.target.value })}
         />
       </DeliveryDetailsForm>
