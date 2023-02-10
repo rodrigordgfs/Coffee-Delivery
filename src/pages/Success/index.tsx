93ab810d2c24e7034e071ed07352f62e2b8fe99b
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-02-10 12:32:28 -0300

    Feature: Criado a tela de Success

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,12 +1,57 @@
-import { SuccessContainer, SuccessWrapper } from "./styles";
+import {
+  OrderDetails,
+  OrderDetailsImage,
+  OrderDetailsList,
+  OrderDetailsListItem,
+  OrderDetailsListItemInfo,
+  OrderDetailsSubtitle,
+  OrderDetailsTitle,
+  SuccessContainer,
+  SuccessWrapper,
+} from "./styles";
+import Deliver from "../../assets/deliver.svg";
+import LocationPurple from "../../assets/icons/LocationPurple.svg";
+import StopwatchYellow from "../../assets/icons/StopwatchYellow.svg";
+import DollarYellowDark from "../../assets/icons/DollarYellowDark.svg";
 
 export function Success() {
   return (
     <SuccessContainer>
       <SuccessWrapper>
-        <div>
-          <h1>Success</h1>
-        </div>
+        <OrderDetails>
+          <OrderDetailsTitle>Uhu! pedido confirmado</OrderDetailsTitle>
+          <OrderDetailsSubtitle>
+            Agora é só aguardar que logo o café chegará até você
+          </OrderDetailsSubtitle>
+          <OrderDetailsList>
+            <OrderDetailsListItem>
+              <img src={LocationPurple} />
+              <OrderDetailsListItemInfo>
+                <p>
+                  Entrega em <span>Rua João Daniel Martinelli, 102</span>
+                </p>
+                <span>Farrapos - Porto Alegre, RS</span>
+              </OrderDetailsListItemInfo>
+            </OrderDetailsListItem>
+            <OrderDetailsListItem>
+              <img src={StopwatchYellow} />
+              <OrderDetailsListItemInfo>
+                <p>Previsão de entrega</p>
+                <span>20 min - 30 min </span>
+              </OrderDetailsListItemInfo>
+            </OrderDetailsListItem>
+            <OrderDetailsListItem>
+              <img src={DollarYellowDark} />
+              <OrderDetailsListItemInfo>
+                <p>Pagamento na entrega</p>
+                <span>Cartão de Crédito</span>
+              </OrderDetailsListItemInfo>
+            </OrderDetailsListItem>
+          </OrderDetailsList>
+        </OrderDetails>
+        <OrderDetailsImage>
+          <img src={Deliver} />
+        </OrderDetailsImage>
       </SuccessWrapper>
     </SuccessContainer>
   );
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -14,10 +14,91 @@ export const SuccessWrapper = styled.section`
   width: 100%;
   max-width: 1200px;
   padding: 0 0.5rem;
-  margin-top: 2.5rem;
+  margin-top: 80px;
   gap: 2rem;
 
   @media only screen and (max-device-width: 1024px) {
     flex-direction: column;
+  }
+`;
+
+export const OrderDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+export const OrderDetailsTitle = styled.h1`
+  font-family: "Baloo 2", sans-serif;
+  font-size: 2rem;
+  line-height: 130%;
+  font-weight: 900;
+  color: ${({ theme }) => theme["yellow-dark"]};
+
+  @media only screen and (max-device-width: 510px) {
+    text-align: center;
+  }
+`;
+
+export const OrderDetailsSubtitle = styled.p`
+  font-family: "Roboto", sans-serif;
+  font-size: 1.25rem;
+  line-height: 130%;
+  font-weight: 400;
+  color: ${({ theme }) => theme.subtitle};
+  margin-top: 0.25rem;
+
+  @media only screen and (max-device-width: 510px) {
+    text-align: center;
+  }
+`;
+
+export const OrderDetailsList = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid ${({ theme }) => theme["yellow-dark"]};
+  border-radius: 0.375rem 2.25rem;
+  padding: 2.5rem;
+  margin-top: 2.5rem;
+  gap: 2rem;
+`;
+
+export const OrderDetailsListItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.75rem;
+
+  @media only screen and (max-device-width: 510px) {
+    flex-direction: column;
+  }
+`;
+
+export const OrderDetailsListItemInfo = styled.div`
+  p {
+    font-family: "Roboto", sans-serif;
+    font-size: 1rem;
+    line-height: 130%;
+    font-weight: 400;
+    color: ${({ theme }) => theme.text};
+  }
+
+  span {
+    font-weight: 700;
+  }
+
+  @media only screen and (max-device-width: 510px) {
+    text-align: center;
+  }
+`;
+
+export const OrderDetailsImage = styled.div`
+  flex: 1;
+  display: flex;
+  items-align: center;
+  justify-content: center;
+
+  img {
+    width: 75%;
   }
 `;
