5067bcefebb3bdfc0bc9153c6309b54786f00fc9
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-02-12 23:59:23 -0300

    Feature: Melhoria na seleção de cafés

--- src/utils/currencyFormat.ts
+++ src/utils/currencyFormat.ts
@@ -1,7 +1,12 @@
-const currencyBRL = new Intl.NumberFormat("pt-BR", {
+const formatCurrencyBRL = new Intl.NumberFormat("pt-BR", {
   style: "currency",
   currency: "BRL",
   minimumFractionDigits: 2,
 });
 
-export default currencyBRL;
+const formatBRL = new Intl.NumberFormat("pt-BR", {
+  currency: "BRL",
+  minimumFractionDigits: 2,
+});
+
+export { formatCurrencyBRL, formatBRL };
